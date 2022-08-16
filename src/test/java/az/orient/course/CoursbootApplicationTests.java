ba004b821cef7fb41c83ef423e23d448fd6e51d9
Shamsiazizaliyev <samsiazizaliyev@gmail.com>
2022-08-16 15:50:07 +0400

    repoziter

---
+++ src/test/java/az/orient/course/CoursbootApplicationTests.java
@@ -0,0 +1,13 @@
+package az.orient.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
